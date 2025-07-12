15d4c17ac7aef14fdcec779916c1ddd4b7c256a3
wongbingg <wb277@naver.com>
2025-07-12 12:58:44 +0900

    feat: SwiftUI 프로젝트 생성

---
+++ NewsLetter/NewsLetter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsLetter
+//
+//  Created by 이원빈 on 7/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsLetter/NewsLetter/NewsLetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsLetterApp.swift
+//  NewsLetter
+//
+//  Created by 이원빈 on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsLetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
