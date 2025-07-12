04dcc4272465b45517d85614aba77dbe5c6dcc9e
LeeJoEun-01 <joeun1005@naver.com>
2025-07-12 20:10:09 +0900

    feat: test용 UI 작성

--- NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeView.swift
+++ NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeView.swift
@@ -5,3 +5,23 @@
 //  Created by 이조은 on 7/12/25.
 //
 
+import SwiftUI
+
+struct HomeView: View {
+  var body: some View {
+      VStack{
+          Rectangle()
+              .fill(.blue)
+              .frame(width: 50, height: 50)
+          Rectangle()
+              .fill(.red)
+              .frame(width: 50, height: 50)
+      }
+  }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
