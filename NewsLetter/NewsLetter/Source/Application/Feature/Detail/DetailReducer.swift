03628a14482294661531b07552a3fe6c2ed7d7c0
LeeJoEun-01 <joeun1005@naver.com>
2025-07-15 00:02:32 +0900

    [#2] feat: Home -> Detail 이동 초기 템플릿 구현

--- NewsLetter/NewsLetter/Source/Application/Feature/Detail/DetailReducer.swift
+++ NewsLetter/NewsLetter/Source/Application/Feature/Detail/DetailReducer.swift
@@ -5,3 +5,31 @@
 //  Created by 이조은 on 7/12/25.
 //
 
+import ComposableArchitecture
+
+@Reducer
+struct DetailReducer {
+    @ObservableState
+    struct State {
+        var title: String = "애플 ‘비밀번호 앱’ 서버를 스위프트로 바꿨더니"
+    }
+
+    enum Action: BindableAction {
+        case binding(BindingAction<State>)
+        case onAppear
+    }
+
+    @Dependency(\.dismiss) var dismiss
+
+    var body: some ReducerOf<Self> {
+        BindingReducer()
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                return .none
+            default:
+                return .none
+            }
+        }
+    }
+}
--- NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeView.swift
+++ NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeView.swift
@@ -5,3 +5,44 @@
 //  Created by 이조은 on 7/12/25.
 //
 
+import SwiftUI
+
+import ComposableArchitecture
+
+struct HomeView: View {
+    @Bindable var store: StoreOf<HomeReducer>
+
+     var body: some View {
+         NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
+             VStack {
+                 Text("Welcome, \(store.userName)")
+                     .font(.headline)
+
+                 Button(action: {
+                     store.send(.detailPressed)
+                 }, label:{
+                     Text("Go to DetailView")
+                         .foregroundStyle(.white)
+                         .frame(width: 200, height: 42, alignment: .center)
+                         .background(.blue)
+                         .cornerRadius(8)
+                 })
+             }
+             .padding()
+         } destination: { store in
+             switch store.case {
+             case .detail(let store):
+                 DetailView(store: store)
+             }
+         }
+         .onAppear {
+             store.send(.onAppear)
+         }
+     }
+}
+
+#Preview {
+    HomeView(store: Store(initialState: HomeReducer.State()) {
+        HomeReducer()
+    })
+}
