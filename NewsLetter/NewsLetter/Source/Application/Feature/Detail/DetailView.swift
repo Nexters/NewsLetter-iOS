923db46d17ef652cbb3ef51c96bc254c896d84df
LeeJoEun-01 <joeun1005@naver.com>
2025-07-15 00:02:32 +0900

    [#6] feat: Home -> Detail 이동 초기 템플릿 구현

--- NewsLetter/NewsLetter/Source/Application/Feature/Detail/DetailView.swift
+++ NewsLetter/NewsLetter/Source/Application/Feature/Detail/DetailView.swift
@@ -5,3 +5,20 @@
 //  Created by 이조은 on 7/12/25.
 //
 
+import SwiftUI
+
+import ComposableArchitecture
+
+struct DetailView: View {
+    @Bindable var store: StoreOf<DetailReducer>
+
+    var body: some View {
+        Text("오늘의 뉴스 기사 제목: \n\(store.title)")
+    }
+}
+
+#Preview {
+    DetailView(store: Store(initialState: DetailReducer.State()) {
+        DetailReducer()
+    })
+}
--- NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeReducer.swift
+++ NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeReducer.swift
@@ -5,3 +5,45 @@
 //  Created by 이조은 on 7/12/25.
 //
 
+import ComposableArchitecture
+
+@Reducer
+struct HomeReducer {
+    @Reducer
+    enum Path {
+        case detail(DetailReducer)
+    }
+
+    @ObservableState
+    struct State {
+        var path = StackState<Path.State>()
+        var userName: String = "조은"
+    }
+
+    enum Action: BindableAction {
+        case binding(BindingAction<State>)
+        case path(StackActionOf<Path>)
+        case onAppear
+        case detailPressed
+    }
+
+    var body: some Reducer<State, Action> {
+        BindingReducer()
+
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                print("HomeView Appear")
+                return .none
+
+            case .detailPressed:
+                state.path.append(.detail(DetailReducer.State()))
+                return .none
+
+            default:
+                return .none
+            }
+        }
+        .forEach(\.path, action: \.path)
+    }
+}
