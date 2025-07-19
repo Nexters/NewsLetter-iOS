2bfb02fd20ca9f982f1dc74023ef717cb41d141f
wongbingg <wb277@naver.com>
2025-07-19 16:18:22 +0900

    [#5] feat: Figma 정의된 컬러 적용

---
+++ NewsLetter/NewsLetter/Source/DesignSystem/Color/AccentColor.swift
@@ -0,0 +1,16 @@
+//
+//  AccentColor.swift
+//  NewsLetter
+//
+//  Created by 이원빈 on 7/19/25.
+//
+
+import SwiftUICore
+
+struct AccentColor {
+    let orange = Color(hex: 0xFF7E35)
+    let skyblue = Color(hex: 0x99C9FF)
+    let lemonyellow = Color(hex: 0xE1FC61)
+    let purple = Color(hex: 0xDBB7F4)
+    let green = Color(hex: 0x33FE8B)
+}
