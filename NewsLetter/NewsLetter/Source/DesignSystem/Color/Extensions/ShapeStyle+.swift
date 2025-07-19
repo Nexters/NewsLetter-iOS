23b2f315fe1055fa9fce5e41ffb55a117b54c138
wongbingg <wb277@naver.com>
2025-07-19 16:17:50 +0900

    [#5] feat: Color, ShapeStyle Extension 구현

---
+++ NewsLetter/NewsLetter/Source/DesignSystem/Color/Extensions/ShapeStyle+.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeStyle+.swift
+//  NewsLetter
+//
+//  Created by 이원빈 on 7/19/25.
+//
+
+import SwiftUICore
+
+extension ShapeStyle where Self == Color {
+    static var accentColor: AccentColor {
+        AccentColor()
+    }
+    
+    static var semanticColor: SemanticColor {
+        SemanticColor()
+    }
+}
