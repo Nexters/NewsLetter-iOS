17741d0ebe9f78d19e65fd315c7a1e6a50e7d6f9
wongbingg <wb277@naver.com>
2025-07-26 15:18:34 +0900

    [#11] chore: 코드 들여쓰기 정리

--- NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeView.swift
+++ NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeView.swift
@@ -12,48 +12,48 @@ import ComposableArchitecture
 struct HomeView: View {
     @Bindable var store: StoreOf<HomeReducer>
     @State var isPresented: Bool = false
-
-     var body: some View {
-         NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
-             ZStack {
-                 VStack {
-                     Text("Welcome, \(store.userName)")
-                         .font(.headline)
-                         .foregroundStyle(.semanticColor.text_primary)
-                     
-                     Button(action: {
-                         isPresented = true
-                     }, label:{
-                         Text("Go to DetailView")
-                             .foregroundStyle(.white)
-                             .frame(width: 200, height: 42, alignment: .center)
-                             .background(.accentColor.orange)
-                             .cornerRadius(8)
-                     })
-                     
-                     Text("IT 직장인이라면 알아야 할 주 4일제의 모든 것을 알려준다")
-                         .font(.caption12_bold)
-                         .frame(width: 179)
-                         .foregroundStyle(ColorPalette.gray600)
-                 }
-                 .padding()
-                 
-                 if isPresented {
-                     CarouselModalView(isPresented: $isPresented)
-                         .transition(.opacity)
-                 }
-             }
-             .animation(.easeInOut, value: isPresented)
-         } destination: { store in
-             switch store.case {
-             case .detail(let store):
-                 DetailView(store: store)
-             }
-         }
-         .onAppear {
-             store.send(.onAppear)
-         }
-     }
+    
+    var body: some View {
+        NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
+            ZStack {
+                VStack {
+                    Text("Welcome, \(store.userName)")
+                        .font(.headline)
+                        .foregroundStyle(.semanticColor.text_primary)
+                    
+                    Button(action: {
+                        isPresented = true
+                    }, label:{
+                        Text("Go to DetailView")
+                            .foregroundStyle(.white)
+                            .frame(width: 200, height: 42, alignment: .center)
+                            .background(.accentColor.orange)
+                            .cornerRadius(8)
+                    })
+                    
+                    Text("IT 직장인이라면 알아야 할 주 4일제의 모든 것을 알려준다")
+                        .font(.caption12_bold)
+                        .frame(width: 179)
+                        .foregroundStyle(ColorPalette.gray600)
+                }
+                .padding()
+                
+                if isPresented {
+                    CarouselModalView(isPresented: $isPresented)
+                        .transition(.opacity)
+                }
+            }
+            .animation(.easeInOut, value: isPresented)
+        } destination: { store in
+            switch store.case {
+            case .detail(let store):
+                DetailView(store: store)
+            }
+        }
+        .onAppear {
+            store.send(.onAppear)
+        }
+    }
 }
 
 #Preview {
