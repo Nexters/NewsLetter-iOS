0d97227838b47ef4cf5ee60fe0833a8b344bdbac
wongbingg <wb277@naver.com>
2025-07-26 12:46:16 +0900

    [#11] feat: DesignSystem Color 추가

--- NewsLetter/NewsLetter/Source/DesignSystem/Color/AccentColor.swift
+++ NewsLetter/NewsLetter/Source/DesignSystem/Color/AccentColor.swift
@@ -5,12 +5,13 @@
 //  Created by 이원빈 on 7/19/25.
 //
 
-import SwiftUICore
+//import SwiftUICore
 
 struct AccentColor {
-    let orange = Color(hex: 0xFF7E35)
-    let skyblue = Color(hex: 0x99C9FF)
-    let lemonyellow = Color(hex: 0xE1FC61)
-    let purple = Color(hex: 0xDBB7F4)
-    let green = Color(hex: 0x33FE8B)
+    let orange = ColorPalette.pointOrange400
+    let skyblue = ColorPalette.pointBlue300
+    let lemonyellow = ColorPalette.pointLemonYellow300
+    let purple = ColorPalette.pointPurple200
+    let pink = ColorPalette.pointPink300
+    let green = ColorPalette.pointGreen300
 }
