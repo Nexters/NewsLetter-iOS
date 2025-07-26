384dd624a128519116657cf6b369c05d0e358d1f
LeeJoEun-01 <joeun1005@naver.com>
2025-07-26 14:04:49 +0900

    [#12] feat: CardView 레이아웃 변경

--- NewsLetter/NewsLetter/Source/Application/Feature/Home/Component/CardView.swift
+++ NewsLetter/NewsLetter/Source/Application/Feature/Home/Component/CardView.swift
@@ -9,21 +9,21 @@ import SwiftUI
 
 enum CardType {
     case one, two, three, four, five, six
-
+    
     var topPadding: CGFloat {
         switch self {
         case .one: return 16
         case .two, .three, .four, .five, .six: return 20
         }
     }
-
+    
     var bottomPadding: CGFloat {
         switch self {
         case .one: return 12
         case .two, .three, .four, .five, .six: return 16
         }
     }
-
+    
     var sidePadding: CGFloat {
         switch self {
         case .one: return 80
@@ -34,18 +34,7 @@ enum CardType {
         case .six: return 0
         }
     }
-
-    var width: CGFloat {
-        switch self {
-        case .one: return 215
-        case .two: return 247
-        case .three: return 279
-        case .four: return 311
-        case .five: return 343
-        case .six: return 375
-        }
-    }
-
+    
     var backgroundColor: Color {
         switch self {
         case .one: return .accentColor.purple
@@ -56,7 +45,7 @@ enum CardType {
         case .six: return .accentColor.green
         }
     }
-
+    
     var fontName: FontStyle {
         switch self {
         case .one: return .body13_bold
@@ -66,7 +55,7 @@ enum CardType {
         case .five, .six: return .body18_bold
         }
     }
-
+    
     var oneLine: Int {
         switch self {
         case .one:   return 15
@@ -84,20 +73,20 @@ struct CardView: View {
     let title: String
     let category: String
     let source: String
-
+    
     @State private var contentHeight: CGFloat = 0
-
+    
     // 제목 최대 28글자까지
     var trimmedTitle: String {
         String(title.prefix(28))
     }
-
+    
     // title이 몇 줄이 되는지 계산
     var titleLineCount: Int {
         let lines = Double(title.count) / Double(cardType.oneLine)
         return Int(ceil(lines))
     }
-
+    
     // 크기가 작은 3종류는 제목이 2줄이면 카테고리랑 출처가 없어야 한다.
     var hideCategoryAndSource: Bool {
         switch cardType {
@@ -107,31 +96,31 @@ struct CardView: View {
             return false
         }
     }
-
+    
     @State private var vStackHeight: CGFloat = 0
-
+    
     var body: some View {
         ZStack(alignment: .topLeading) {
             RoundedRectangle(cornerRadius: 24)
                 .foregroundStyle(cardType.backgroundColor)
-
+            
             VStack(alignment: .leading, spacing: 0) {
                 Text(trimmedTitle)
                     .font(cardType.fontName)
                     .foregroundStyle(.semanticColor.text_strong)
                     .padding(.top, hideCategoryAndSource ? 16 : cardType.topPadding)
                     .padding(.bottom, hideCategoryAndSource ? cardType.bottomPadding : 4)
-
+                
                 if !hideCategoryAndSource {
                     HStack(spacing: 6) {
                         Text(category)
                             .font(.body13_medium)
                             .foregroundStyle(.semanticColor.text_strong.opacity(0.5))
-
+                        
                         Rectangle()
                             .frame(width: 1, height: 14)
                             .foregroundStyle(ColorPalette.black.opacity(0.1))
-
+                        
                         Text(source)
                             .font(.body13_medium)
                             .foregroundStyle(.semanticColor.text_strong.opacity(0.5))
@@ -147,26 +136,24 @@ struct CardView: View {
                         .onAppear {
                             self.vStackHeight = proxy.size.height
                         }
+                        .onChange(of: proxy.size.height) { newValue in
+                            self.vStackHeight = newValue
+                        }
                 }
             )
         }
-        .frame(width: cardType.width, height: vStackHeight == 0 ? nil : vStackHeight + 30)
+        .padding([.leading, .trailing], cardType.sidePadding)
+        .frame(height: vStackHeight == 0 ? nil : vStackHeight + 30)
     }
-
 }
 
 #Preview {
-    VStack(spacing:0) {
+    VStack(spacing:-30) {
         CardView(cardType: .one, title: "메가커피 컵빙수의 품절 대란", category: "Kotlin", source: "안드로이드 위클리")
-            .offset(y:150)
         CardView(cardType: .two, title: "일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔", category: "Kotlin", source: "안드로이드 위클리")
-            .offset(y:120)
         CardView(cardType: .three, title: "일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔", category: "Kotlin", source: "안드로이드 위클리")
-            .offset(y:90)
         CardView(cardType: .four, title: "직장인이라면 알아야 할 주 4일제의 모든 것", category: "Kotlin", source: "안드로이드 위클리")
-            .offset(y:60)
         CardView(cardType: .five, title: "이재명 정부 부동산 규제, 더 큰 게 온다? 전 대통령, 구속영장", category: "Kotlin", source: "안드로이드 위클리")
-            .offset(y:30)
         CardView(cardType: .six, title: "재구속 앞둔 윤석열 전 대통령, 구속영장은?", category: "Kotlin", source: "안드로이드 위클리")
     }
 }
