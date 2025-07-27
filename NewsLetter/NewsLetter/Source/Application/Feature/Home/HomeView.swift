b96e2d1d804d9dd7f6ffa59b9b316460fe25e2f5
wongbingg <wb277@naver.com>
2025-07-27 20:44:05 +0900

    [#16] feat: HomeView 에 CarouselModalView 인터렉션 추가
    - 선택된 카드 index 를 기반으로 scrollPosition을 설정하도록 적용

--- NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeView.swift
+++ NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeView.swift
@@ -11,47 +11,60 @@ import ComposableArchitecture
 
 struct HomeView: View {
     @Bindable var store: StoreOf<HomeReducer>
+    @State private var isPresentModal: Bool = false
+    @State private var selectedIndex: Int?
     
     var body: some View {
         NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
-            VStack {
-                Text("\(store.state.todayDate)\nToday’s Hot News")
-                    .font(Font.custom("Jalnan Gothic", size: 32))
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.semanticColor.text_strong)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .padding(.top, 44)
-                    .fixedSize(horizontal: false, vertical: true)
-
-                Text(store.state.formattedTime)
-                    .font(.body16_semiBold)
-                    .foregroundColor(.semanticColor.state_negative_primary)
-                    .padding(.top, 8)
-
-                Spacer()
-
-                VStack(spacing: -35) {
-                    ForEach(store.state.cardData, id: \.0) { type, title, category, source in
-                        CardView(
-                            cardType: type,
-                            title: title,
-                            category: category,
-                            source: source,
-                            onTap: {
-                                print("hello")
-                            }
-                        )
+            ZStack {
+                VStack {
+                    Text("\(store.state.todayDate)\nToday’s Hot News")
+                        .font(Font.custom("Jalnan Gothic", size: 32))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.semanticColor.text_strong)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(.top, 44)
+                        .fixedSize(horizontal: false, vertical: true)
+                    
+                    Text(store.state.formattedTime)
+                        .font(.body16_semiBold)
+                        .foregroundColor(.semanticColor.state_negative_primary)
+                        .padding(.top, 8)
+                    
+                    Spacer()
+                    
+                    VStack(spacing: -35) {
+                        ForEach(store.state.cardData, id: \.0) { type, title, category, source in
+                            CardView(
+                                cardType: type,
+                                title: title,
+                                category: category,
+                                source: source,
+                                onTap: {
+                                    // FIXME: 추후 카드 데이터 순서 논의 필요
+                                    selectedIndex = store.state.cardData.reversed().firstIndex(where: { $0.0 == type })
+                                    isPresentModal = true
+                                }
+                            )
+                        }
                     }
+                    .padding(.bottom, -20)
                 }
-                .padding(.bottom, -20)
+                .ignoresSafeArea(edges: .bottom)
+                .transition(.opacity)
+                .onAppear {
+                    store.send(.onAppear)
+                }
+                .onDisappear {
+                    store.send(.onDisappear)
+                }
+                
+                if isPresentModal {
+                    CarouselModalView(isPresented: $isPresentModal, currentPage: $selectedIndex)
+                        .transition(.opacity)
+                }
             }
-            .ignoresSafeArea(edges: .bottom)
-            .onAppear {
-                store.send(.onAppear)
-            }
-            .onDisappear {
-                store.send(.onDisappear)
-            }
+            .animation(.easeInOut, value: isPresentModal)
         } destination: { store in
             switch store.case {
             case .detail(let store):
