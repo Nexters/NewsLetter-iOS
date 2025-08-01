91a9a489c1dc507709860762238b30203988127f
LeeJoEun-01 <joeun1005@naver.com>
2025-08-01 21:29:34 +0900

    [#23] feat: CardAPI 통신 코드 구현
    - CardAPI, CardDTO, CardClient

---
+++ NewsLetter/NewsLetter/Source/Data/Client/CardClient.swift
@@ -0,0 +1,65 @@
+//
+//  CardClient.swift
+//  NewsLetter
+//
+//  Created by 이조은 on 8/1/25.
+//
+
+import Combine
+
+import ComposableArchitecture
+import Moya
+
+@DependencyClient
+struct CardClient {
+    static let apiClient = MoyaAPIClient()
+
+    var fetchCards: (String, String?) async throws -> [Card]
+}
+
+extension DependencyValues {
+    var cardClient: CardClient {
+        get { self[CardClient.self] }
+        set { self[CardClient.self] = newValue }
+    }
+}
+
+extension CardClient: DependencyKey {
+    static var liveValue: CardClient = {
+            return CardClient(
+                fetchCards: { userId, publishedDate in
+                    let response = try await apiClient.request(CardAPI.fetchCards(userId: userId, publishedDate: publishedDate))
+
+                    let cardResponse = try response.map(CardResponseDTO.self)
+                    let cards = cardResponse.cards.map { cardDTO in
+                        Card(
+                            title: cardDTO.title,
+                            topKeyword: cardDTO.topKeyword,
+                            summary: cardDTO.summary,
+                            contentURL: cardDTO.contentURL,
+                            newsletterName: cardDTO.newsletterName
+                        )
+                    }
+                    return cards
+                }
+            )
+        }()
+
+    static var previewValue: CardClient = {
+        return CardClient(
+            fetchCards: { _,_  in
+                return [
+                    Card(title: "Preview Title 1", topKeyword: "Preview Keyword 1", summary: "Preview Summary 1", contentURL: "https://example.com", newsletterName: "Preview Newsletter 1"),
+                    Card(title: "Preview Title 2", topKeyword: "Preview Keyword 2", summary: "Preview Summary 2", contentURL: "https://example.com", newsletterName: "Preview Newsletter 2"),
+                    Card(title: "Preview Title 3", topKeyword: "Preview Keyword 3", summary: "Preview Summary 3", contentURL: "https://example.com", newsletterName: "Preview Newsletter 3"),
+                    Card(title: "Preview Title 4", topKeyword: "Preview Keyword 4", summary: "Preview Summary 4", contentURL: "https://example.com", newsletterName: "Preview Newsletter 4"),
+                    Card(title: "Preview Title 5", topKeyword: "Preview Keyword 5", summary: "Preview Summary 5", contentURL: "https://example.com", newsletterName: "Preview Newsletter 5"),
+                    Card(title: "Preview Title 6", topKeyword: "Preview Keyword 6", summary: "Preview Summary 6", contentURL: "https://example.com", newsletterName: "Preview Newsletter 6")
+                ]
+            }
+        )
+    }()
+
+    static var testValue: CardClient = previewValue
+}
+
--- NewsLetter/NewsLetter/Source/Domain/Entity/Card.swift
+++ NewsLetter/NewsLetter/Source/Domain/Entity/Card.swift
@@ -7,18 +7,25 @@
 
 struct Card {
     let title: String
-    let keyword: String
-    let description: String
-    
+    let topKeyword: String
+    let summary: String
+    let contentURL: String
+    let newsletterName: String
+
+    // Card를 스텁 데이터로 초기화하는 함수
     static func stub(
         title: String = "일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔",
-        keyword: String = "Kotlin | 안드로이드 위클리",
-        description: String = "Anatolii Frolov는 Kotlin 객체 싱글톤이 Gson과 같은 라이브러리에 의해 복제될 수 있으므로 역직렬화할 때 실제 싱글톤 동작을 유지하려면 사용자 정의 어댑터가 필요하다고 강조합니다.Anatolii Frolov는 Kotlin 객체 싱글톤이 Gson과 같은 라이브러리에 의해 복제될 수 있으므로 역렬화할 때 실제 싱글톤 동작을 강조합니다."
+        topKeyword: String = "Kotlin | 안드로이드 위클리",
+        summary: String = "Anatolii Frolov는 Kotlin 객체 싱글톤이 Gson과 같은 라이브러리에 의해 복제될 수 있으므로 역직렬화할 때 실제 싱글톤 동작을 유지하려면 사용자 정의 어댑터가 필요하다고 강조합니다.Anatolii Frolov는 Kotlin 객체 싱글톤이 Gson과 같은 라이브러리에 의해 복제될 수 있으므로 역렬화할 때 실제 싱글톤 동작을 강조합니다.",
+        contentURL: String = "https://substack.com/redirect/3966a1cb-5964-4e4c-a46c-67fd93586b71?j=eyJ1IjoiNjBoYXcyIn0.sgwjhuRgLj2i1p7EiKYAd1HGVttvTH1zz-7ivhBT090",
+        newsletterName: String = "Kotlin Article"
     ) -> Self {
-        .init(
+        return .init(
             title: title,
-            keyword: keyword,
-            description: description
+            topKeyword: topKeyword,
+            summary: summary,
+            contentURL: contentURL,
+            newsletterName: newsletterName
         )
     }
 }
