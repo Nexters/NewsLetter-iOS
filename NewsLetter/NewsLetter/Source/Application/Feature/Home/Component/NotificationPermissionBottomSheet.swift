f98106303bd0b5cf9b0bea657755715c3d45ec4d
wongbingg <wb277@naver.com>
2025-08-01 18:28:16 +0900

    [#21] feat: NotificationPermissionBottomSheet 구현
    - API 연동 추후 구현

---
+++ NewsLetter/NewsLetter/Source/Application/Feature/Home/Component/NotificationPermissionBottomSheet.swift
@@ -0,0 +1,84 @@
+//
+//  NotificationPermissionBottomSheet.swift
+//  NewsLetter
+//
+//  Created by 이원빈 on 7/30/25.
+//
+
+import SwiftUI
+
+struct NotificationPermissionBottomSheet: View {
+    @Environment(\.scenePhase) private var scenePhase
+    @Binding var isPresented: Bool
+    @State private var isCheckingPermission = false
+    
+    let successHandler: (() -> Void)
+    
+    var body: some View {
+        VStack {
+            Text("가장 먼저 새로운 소식을 받아보세요")
+                .font(.head22_bold)
+                .foregroundStyle(.semanticColor.text_strong)
+            
+            Text("매일 유용한 뉴스레터를 보내드려요.")
+                .font(.body16_regular)
+                .foregroundStyle(.semanticColor.text_tertiary)
+                .padding(.top, 8)
+            
+            Button(action: {
+                checkNotificationPermission()
+            }) {
+                RoundedRectangle(cornerRadius: 100)
+                    .frame(height: 56)
+                    .foregroundStyle(.semanticColor.fill_primaryInversion)
+                    .padding(.horizontal, 16)
+                    .overlay {
+                        Text("알림 받기")
+                            .font(.body16_semiBold)
+                            .foregroundStyle(.semanticColor.text_strongInverse)
+                    }
+            }
+            .padding(.top, 32)
+        }
+        .onChange(of: scenePhase) { _, newPhase in
+            if newPhase == .active && isCheckingPermission {
+                checkNotificationStatusAndDismissIfAllowed()
+            }
+        }
+    }
+    
+    private func checkNotificationPermission() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            DispatchQueue.main.async {
+                if settings.authorizationStatus == .authorized {
+                    UserActionHistory.isAlreadySetNotification = true
+                    successHandler()
+                    // TODO: 알림등록 API 요청
+                    isPresented = false
+                } else {
+                    isCheckingPermission = true
+                    guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+                    
+                    if UIApplication.shared.canOpenURL(url){
+                        UIApplication.shared.open(url)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func checkNotificationStatusAndDismissIfAllowed() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            DispatchQueue.main.async {
+                if settings.authorizationStatus == .authorized {
+                    isPresented = false
+                }
+                isCheckingPermission = false
+            }
+        }
+    }
+}
+
+#Preview {
+    NotificationPermissionBottomSheet(isPresented: .constant(true), successHandler: {})
+}
