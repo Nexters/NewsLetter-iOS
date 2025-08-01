a6d185ac44c34694e1019c7cb56127ff3c5e1659
wongbingg <wb277@naver.com>
2025-08-01 18:33:14 +0900

    [#21] feat: HomeView 에 BottomSheet UI 및 정책 적용
    - API 연동 추후 구현

--- NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeView.swift
+++ NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeView.swift
@@ -13,7 +13,11 @@ struct HomeView: View {
     @Bindable var store: StoreOf<HomeReducer>
     let colorFlag: String
     @State private var isPresentModal: Bool = false
+    @State private var isPresentJobDetailBottomSheet: Bool = false
+    @State private var isPresentNotificationPermissionBottomSheet: Bool = false
+    @State private var isPresentToastMessage: Bool = false
     @State private var selectedIndex: Int?
+    @State private var cardTapCount: Int = 0
     
     var body: some View {
         NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
@@ -26,14 +30,14 @@ struct HomeView: View {
                         .frame(maxWidth: .infinity, alignment: .center)
                         .padding(.top, 44)
                         .fixedSize(horizontal: false, vertical: true)
-
+                    
                     Text(store.state.formattedTime)
                         .font(.body16_semiBold)
                         .foregroundColor(.semanticColor.state_negative_primary)
                         .padding(.top, 8)
-
+                    
                     Spacer()
-
+                    
                     if store.cardColors.count == store.cardData.count {
                         VStack(spacing: -35) {
                             ForEach(Array(store.state.cardData.enumerated()), id: \.offset) { index, item in
@@ -47,7 +51,20 @@ struct HomeView: View {
                                     onTap: {
                                         // FIXME: 추후 카드 데이터 순서 논의 필요
                                         selectedIndex = store.state.cardData.reversed().firstIndex(where: { $0.0 == type })
-                                        isPresentModal = true
+                                        
+                                        if cardTapCount >= 3 {
+                                            guard UserActionHistory.isAlreadyInputJobDetail == false &&
+                                                    DateCalculator.isCanShowJobDetailBottomSheet()
+                                            else {
+                                                isPresentModal = true
+                                                return
+                                            }
+                                            
+                                            isPresentJobDetailBottomSheet = true
+                                        } else {
+                                            isPresentModal = true
+                                        }
+                                        cardTapCount += 1
                                     }
                                 )
                             }
@@ -57,15 +74,50 @@ struct HomeView: View {
                 }
                 .ignoresSafeArea(edges: .bottom)
                 .transition(.opacity)
+                .draggableBottomSheet(
+                    isShow: $isPresentJobDetailBottomSheet,
+                    dismissHandler: { UserActionHistory.deniedDateWhenInputJobDetail = Date() }
+                ) {
+                    JobDetailBottomSheet()
+                }
+                .draggableBottomSheet(
+                    isShow: $isPresentNotificationPermissionBottomSheet,
+                    dismissHandler: { UserActionHistory.deniedDateWhenSetNotification = Date() }
+                ) {
+                    NotificationPermissionBottomSheet(
+                        isPresented: $isPresentNotificationPermissionBottomSheet,
+                        successHandler: { isPresentToastMessage = true }
+                    )
+                }
+                .ignoresSafeArea(edges: .bottom)
                 .onAppear {
                     store.send(.onAppear(colorFlag: self.colorFlag))
+                    
+                    DateCalculator.checkAndIncrementVisitStreak()
+                    
+                    guard UserActionHistory.streakCount >= 2 &&
+                            UserActionHistory.isAlreadySetNotification == false &&
+                            DateCalculator.isCanShowNotificationPermissionBottomSheet()
+                    else { return }
+                    
+                    isPresentNotificationPermissionBottomSheet = true
                 }
                 .onDisappear {
                     store.send(.onDisappear)
                 }
+                .toastMessage(
+                    isPresented: $isPresentToastMessage,
+                    text: "뉴스레터 알림이 신청되었어요",
+                    bottomPadding: 0
+                )
+                
                 if isPresentModal {
-                    CarouselModalView(isPresented: $isPresentModal, currentPage: $selectedIndex)
-                        .transition(.opacity)
+                    CarouselModalView(
+                        isPresented: $isPresentModal,
+                        currentPage: $selectedIndex,
+                        firstLookHandler: { isPresentNotificationPermissionBottomSheet = true }
+                    )
+                    .transition(.opacity)
                 }
             }
             .animation(.easeInOut, value: isPresentModal)
