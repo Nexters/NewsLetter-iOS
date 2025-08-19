40c51f01da0d897e2c610e7ae609af3527d1299d
wongbingg <wb277@naver.com>
2025-08-19 22:30:59 +0900

    [#55] feat: 카드 뽑기 애니메이션 추가
    - 하드코딩됨

--- NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeView.swift
+++ NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeView.swift
@@ -25,7 +25,7 @@ struct HomeView: View {
     
     var body: some View {
         NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
-            ZStack {
+            ZStack(alignment: .bottom) {
                 VStack {
                     HStack {
                         Spacer()
@@ -65,119 +65,45 @@ struct HomeView: View {
 
                     Spacer()
 
-                    VStack(spacing: -35) {
-                        ForEach(Array(store.state.cardData.enumerated()), id: \.offset) { index, item in
-                            CardView(
-                                cardType: cardTypes[index],
-                                color: store.cardColors[index],
-                                title: item.title,
-                                category: item.topKeyword,
-                                source: item.newsletterName,
-                                onTap: {
-                                    selectedIndex = index
-                                    cardTapHandler()
-
-                                    let dataString = (try? JSONSerialization.data(withJSONObject: ["list_index": store.state.cardData.count-1-index]))
-                                        .flatMap { String(data: $0, encoding: .utf8) }
-
-                                    Analytics.logEvent("click_newsletter", parameters: [
-                                        "category": "click",
-                                        "navigation": "main",
-                                        "object_section": "newsletter_list",
-                                        "object_type": "newsletter",
-                                        "object_id": item.title,
-                                        "data": dataString ?? ""
-                                    ])
-                                }
-                            )
-                            .offset(y: pulseOffsets[index] ?? 0)
-                            .onAppear {
-                                guard !didAnimateIndex.contains(index) else { return }
-                                didAnimateIndex.insert(index)
-
-                                let playOrder = (store.state.cardData.count - 1) - index
-                                let delayMs = playOrder * 150
-
-                                DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(delayMs)) {
-                                    withAnimation(.easeInOut(duration: 0.2)) {
-                                        pulseOffsets[index] = -5
-                                    }
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(200)) {
-                                        withAnimation(.easeInOut(duration: 0.1)) {
-                                            pulseOffsets[index] = 0
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    .padding(.bottom, -20)
-                    .background(
-                        Image("bg_drawers")
-                            .resizable()
-                            .scaledToFill()
-                            .padding(.top, -25)
-                    )
+                    Image("bg_drawers")
+                        .resizable()
+                        .frame(width: 600, height: 526)
                 }
-                .ignoresSafeArea(edges: .all)
-                .transition(.opacity)
-                .draggableBottomSheet(
-                    isShow: $isPresentJobDetailBottomSheet,
-                    dismissHandler: { UserActionHistory.deniedDateWhenInputJobDetail = Date() }
-                ) {
-                    JobDetailBottomSheet { selectedJobCategory, selectedCareer in
-                        jobDetailBottomSheetConfirmHandler(
-                            selectedJobCategory: selectedJobCategory,
-                            selectedCareer: selectedCareer
+
+                Group {
+                    ForEach(Array(store.state.cardData.enumerated()), id: \.offset) { index, item in
+                        CardView(
+                            cardType: cardTypes[index],
+                            color: store.cardColors[index],
+                            title: item.title,
+                            category: item.topKeyword,
+                            source: item.newsletterName,
+                            shouldMoveY: ((Device.height - 477) / 2) + 180 - (Device.height - 450 + CGFloat(index * 80)),
+                            /// 477 은 CarouselCard 부터 하단 X 버튼 까지의 높이
+                            /// 200 은 조정값 (position 이 뷰의 중앙을 표현하는 값인 거 같아서 뷰의 높이 절반을 추가해주는 거)
+                            onTap: {
+                                selectedIndex = index
+                                cardTapHandler()
+
+                                let dataString = (try? JSONSerialization.data(withJSONObject: ["list_index": store.state.cardData.count-1-index]))
+                                    .flatMap { String(data: $0, encoding: .utf8) }
+
+                                Analytics.logEvent("click_newsletter", parameters: [
+                                    "category": "click",
+                                    "navigation": "main",
+                                    "object_section": "newsletter_list",
+                                    "object_type": "newsletter",
+                                    "object_id": item.title,
+                                    "data": dataString ?? ""
+                                ])
+                            },
+                            isPresentModal: $isPresentModal,
                         )
+                        .position(x: Device.width / 2, y: Device.height - 450 + CGFloat(index * 80))
                     }
                 }
-                .draggableBottomSheet(
-                    isShow: $isPresentNotificationPermissionBottomSheet,
-                    dismissHandler: { UserActionHistory.deniedDateWhenSetNotification = Date() }
-                ) {
-                    NotificationPermissionBottomSheet(
-                        isPresented: $isPresentNotificationPermissionBottomSheet,
-                        successHandler: {
-                            store.send(.registerUser)
-                            isPresentToastMessage = true
-                        }
-                    )
-                }
-                .ignoresSafeArea(edges: .bottom)
-                .toastMessage(
-                    isPresented: $isPresentToastMessage,
-                    text: "뉴스레터 알림이 신청되었어요",
-                    bottomPadding: 0
-                )
-                .onAppear {
-                    Analytics.logEvent(AnalyticsEventScreenView,
-                   parameters: [
-                    AnalyticsParameterScreenName: "main"
-                   ])
-
-                    store.send(.onAppear(colorFlag: self.colorFlag))
-                    
-                    if UserActionHistory.isFirstAppLaunch {
-                        UserActionHistory.isFirstAppLaunch = false
-                        
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                            isPresentJobDetailBottomSheet = true
-                        }
-                    }
-
-                    DateCalculator.checkAndIncrementVisitStreak()
-                    
-                    guard UserActionHistory.streakCount >= 2 &&
-                            UserActionHistory.isAlreadySetNotification == false &&
-                            DateCalculator.isCanShowNotificationPermissionBottomSheet()
-                    else { return }
-                    
-                    isPresentNotificationPermissionBottomSheet = true
-                }
-                .onDisappear {
-                    store.send(.onDisappear)
-                }
+                .padding(.bottom, -20)
+                .frame(width: Device.width)
                 
                 if isPresentModal {
                     CarouselModalView(
@@ -187,11 +113,71 @@ struct HomeView: View {
                         currentPage: $selectedIndex,
                         firstLookHandler: { isPresentNotificationPermissionBottomSheet = true }
                     )
+                    .frame(width: Device.width)
                     .transition(.opacity)
-                    .zIndex(1)
+                    .zIndex(20)
                 }
             }
             .animation(.easeInOut, value: isPresentModal)
+            .ignoresSafeArea(edges: .all)
+            .transition(.opacity)
+            .draggableBottomSheet(
+                isShow: $isPresentJobDetailBottomSheet,
+                dismissHandler: { UserActionHistory.deniedDateWhenInputJobDetail = Date() }
+            ) {
+                JobDetailBottomSheet { selectedJobCategory, selectedCareer in
+                    jobDetailBottomSheetConfirmHandler(
+                        selectedJobCategory: selectedJobCategory,
+                        selectedCareer: selectedCareer
+                    )
+                }
+            }
+            .draggableBottomSheet(
+                isShow: $isPresentNotificationPermissionBottomSheet,
+                dismissHandler: { UserActionHistory.deniedDateWhenSetNotification = Date() }
+            ) {
+                NotificationPermissionBottomSheet(
+                    isPresented: $isPresentNotificationPermissionBottomSheet,
+                    successHandler: {
+                        store.send(.registerUser)
+                        isPresentToastMessage = true
+                    }
+                )
+            }
+            .ignoresSafeArea(edges: .bottom)
+            .toastMessage(
+                isPresented: $isPresentToastMessage,
+                text: "뉴스레터 알림이 신청되었어요",
+                bottomPadding: 0
+            )
+            .onAppear {
+                Analytics.logEvent(AnalyticsEventScreenView,
+               parameters: [
+                AnalyticsParameterScreenName: "main"
+               ])
+
+                store.send(.onAppear(colorFlag: self.colorFlag))
+                
+                if UserActionHistory.isFirstAppLaunch {
+                    UserActionHistory.isFirstAppLaunch = false
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                        isPresentJobDetailBottomSheet = true
+                    }
+                }
+
+                DateCalculator.checkAndIncrementVisitStreak()
+                
+                guard UserActionHistory.streakCount >= 2 &&
+                        UserActionHistory.isAlreadySetNotification == false &&
+                        DateCalculator.isCanShowNotificationPermissionBottomSheet()
+                else { return }
+                
+                isPresentNotificationPermissionBottomSheet = true
+            }
+            .onDisappear {
+                store.send(.onDisappear)
+            }
         } destination: { store in
             switch store.case {
             case .setting(let store):
