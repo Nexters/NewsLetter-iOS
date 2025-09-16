9882534c510772b6141a199bbf0451e703a94521
wongbingg <wb277@naver.com>
2025-09-16 20:53:53 +0900

    [#69] fix: 타이머 딜레이 이슈 픽스본 원복
    - bottom sheet 터치블락 이슈를 유발했던 부분을 다시 원복

--- NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeReducer.swift
+++ NewsLetter/NewsLetter/Source/Application/Feature/Home/HomeReducer.swift
@@ -118,9 +118,6 @@ struct HomeReducer {
                 state.path.append(.setting(SettingReducer.State()))
                 return .none
             case .startTimer:
-                guard state.timerIsRunning == false else {
-                    return .none
-                }
                 state.timerIsRunning = true
                 state.remainingSeconds = DateCalculator.secondsUntilMidnight(from: self.now)
 
