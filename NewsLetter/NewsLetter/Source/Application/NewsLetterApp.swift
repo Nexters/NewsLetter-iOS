70672ec9dd2f8889687a0cab4253009029a5a177
LeeJoEun-01 <joeun1005@naver.com>
2025-10-12 12:58:59 +0900

    [#81] feat: 카카오 SDK 추가 및 기초 세팅
    - appKey, Schemes 설정

--- NewsLetter/NewsLetter/Source/Application/NewsLetterApp.swift
+++ NewsLetter/NewsLetter/Source/Application/NewsLetterApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 import ComposableArchitecture
 import FirebaseCore
 import FirebaseMessaging
+import KakaoSDKCommon
 
 @main
 struct NewsLetterApp: App {
@@ -39,6 +40,8 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         application.registerForRemoteNotifications()
         Messaging.messaging().delegate = self
         UNUserNotificationCenter.current().delegate = self
+
+        KakaoSDK.initSDK(appKey: "3eb3851f7f6457accc8d6fd4585db5e2")
 
         return true
     }
