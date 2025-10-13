137425c3c475e36492cfe064b268dd7b7747f378
LeeJoEun-01 <joeun1005@naver.com>
2025-10-13 22:00:39 +0900

    [#81] fix: 코드 리뷰 반영

--- NewsLetter/NewsLetter/Source/Application/Feature/Home/Component/CarouselModalView/CarouselCard.swift
+++ NewsLetter/NewsLetter/Source/Application/Feature/Home/Component/CarouselModalView/CarouselCard.swift
@@ -55,40 +55,39 @@ struct CarouselCard: View {
             Spacer()
             
             HStack(spacing: 8) {
-                ShareLink(item: card.contentURL) { // FIXME: item 에 og share url 이 들어가야함
-                    RoundedRectangle(cornerRadius: Metric.shareButtonSize/2)
-                        .stroke(.semanticColor.border_secondary, style: .init(lineWidth: 1))
-                        .background(ColorPalette.white)
-                        .frame(width: Metric.shareButtonSize, height: Metric.shareButtonSize)
-                        .overlay {
-                            Image("share_icon")
-                                .resizable()
-                                .frame(width: 24, height: 24)
-                            Button {
-                                Task {
-                                    await kakaoShareManager.shareToKakao(
-                                        title: card.title,
-                                        id: card.id,
-                                        textColor: pointColor,
-                                        contentURL: card.contentURL
-                                    )
+                RoundedRectangle(cornerRadius: Metric.shareButtonSize/2)
+                    .stroke(.semanticColor.border_secondary, style: .init(lineWidth: 1))
+                    .background(ColorPalette.white)
+                    .frame(width: Metric.shareButtonSize, height: Metric.shareButtonSize)
+                    .overlay {
+                        Image("share_icon")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                        Button {
+                            Task {
+                                await kakaoShareManager.shareToKakao(
+                                    title: card.title,
+                                    id: card.id,
+                                    textColor: pointColor,
+                                    contentURL: card.contentURL
+                                )
+                            }
+                        } label: {
+                            RoundedRectangle(cornerRadius: Metric.shareButtonSize/2)
+                                .stroke(.semanticColor.border_secondary, style: .init(lineWidth: 1))
+                                .background(ColorPalette.white)
+                                .frame(width: Metric.shareButtonSize, height: Metric.shareButtonSize)
+                                .overlay {
+                                    Image("share_icon")
+                                        .resizable()
+                                        .frame(width: 24, height: 24)
+                                        .foregroundStyle(.semanticColor.text_primary)
+
                                 }
-                            } label: {
-                                RoundedRectangle(cornerRadius: Metric.shareButtonSize/2)
-                                    .stroke(.semanticColor.border_secondary, style: .init(lineWidth: 1))
-                                    .background(ColorPalette.white)
-                                    .frame(width: Metric.shareButtonSize, height: Metric.shareButtonSize)
-                                    .overlay {
-                                        Image("share_icon")
-                                            .resizable()
-                                            .frame(width: 24, height: 24)
-                                            .foregroundStyle(.semanticColor.text_primary)
-
-                                    }
-                            }   .foregroundStyle(.semanticColor.text_primary)
-
                         }
-                }
+                        .foregroundStyle(.semanticColor.text_primary)
+                    }
+
 
                 Button {
                     isWebViewPresented = true
