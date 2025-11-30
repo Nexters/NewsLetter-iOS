35355292484cbf0405845d9c4a28165e92490824
wongbingg <wb277@naver.com>
2025-11-30 16:07:46 +0900

    [#90] feat: Home 재구성 및 커스텀 네비게이션바 추가
    - recommend(추천탭) 과 explore(탐색탭) 을 관리하는 컨테이너 Reducer 역할 부여

---
+++ NewsLetter/NewsLetter/Source/Application/Feature/Home/Component/CustomNativationBar.swift
@@ -0,0 +1,58 @@
+//
+//  CustomNativationBar.swift
+//  NewsLetter
+//
+//  Created by 이원빈 on 11/22/25.
+//
+
+import SwiftUI
+
+struct CustomNativationBar: View {
+    private enum Metric {
+        static let fakeViewWidth: CGFloat = 40
+        static let fakeViewHeight: CGFloat = 32
+        static let buttonSize: CGFloat = 24
+        static let barHeight: CGFloat = UIDevice.isSmallScreen ? 36 : 48
+        static let topPadding: CGFloat = UIDevice.isSmallScreen ? 24 : 50
+    }
+    
+    @Binding var selectedSegment: SegmentView.SegmentType
+    let settingButtonTapHandler: (() -> Void)
+    
+    private var foregroundMainColor: Color {
+        selectedSegment == .recommend ? .black : .white
+    }
+    
+    private var backgroundMainColor: Color {
+        selectedSegment == .recommend ? .white : .black
+    }
+    
+    var body: some View {
+        HStack {
+            Rectangle()
+                .frame(width: Metric.fakeViewWidth, height: Metric.fakeViewHeight)
+                .opacity(0)
+            Spacer()
+            SegmentView(selectedSegment: $selectedSegment)
+            Spacer()
+            Button {
+                settingButtonTapHandler()
+            } label: {
+                Image("setting_icon")
+                    .renderingMode(.template)
+                    .resizable()
+                    .frame(width: Metric.buttonSize, height: Metric.buttonSize)
+                    .foregroundColor(foregroundMainColor)
+                    .padding(8)
+                    .padding(.trailing, 8)
+            }
+        }
+        .frame(width: Device.width, height: Metric.barHeight)
+        .padding(.top, Metric.topPadding)
+        .background(backgroundMainColor)
+    }
+}
+
+#Preview {
+    CustomNativationBar(selectedSegment: .constant(.recommend), settingButtonTapHandler: {})
+}
