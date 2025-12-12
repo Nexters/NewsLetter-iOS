3235293b2dba86e2059d153a68e0e999aa3b1626
wongbingg <wb277@naver.com>
2025-12-12 19:35:21 +0900

    [#90] refactor: CarouselCard 모델 수정
    - 사용처에 따른 분기처리를 위해 플래그 변수 isShareEnable 추가 (추천탭: 공유버튼O, 탐색탭: 공유버튼X)

--- NewsLetter/NewsLetter/Source/Application/Feature/Recommend/Component/CarouselModalView/CarouselCard.swift
+++ NewsLetter/NewsLetter/Source/Application/Feature/Recommend/Component/CarouselModalView/CarouselCard.swift
@@ -26,6 +26,7 @@ struct CarouselCard: View {
     let card: Card
     let index: Int
     let pointColor: Color
+    let isShareEnabled: Bool
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
@@ -66,34 +67,40 @@ struct CarouselCard: View {
             Spacer()
             
             HStack(spacing: 8) {
-                Button {
-                    Task {
-                        await kakaoShareManager.shareToKakao(
-                            title: card.title,
-                            id: card.id,
-                            textColor: pointColor,
-                            contentURL: card.contentURL
-                        )
-                    }
-                } label: {
-                    RoundedRectangle(cornerRadius: Metric.shareButtonSize/2)
-                        .stroke(.semanticColor.border_secondary, style: .init(lineWidth: 1))
-                        .background(ColorPalette.white)
-                        .frame(width: Metric.shareButtonSize, height: Metric.shareButtonSize)
-                        .overlay {
-                            Image("share_icon")
-                                .resizable()
-                                .frame(width: 24, height: 24)
-                                .foregroundStyle(.semanticColor.text_primary)
-
+                if isShareEnabled {
+                    Button {
+                        Task {
+                            await kakaoShareManager.shareToKakao(
+                                title: card.title,
+                                id: card.id,
+                                textColor: pointColor,
+                                contentURL: card.contentURL
+                            )
                         }
+                    } label: {
+                        RoundedRectangle(cornerRadius: Metric.shareButtonSize/2)
+                            .stroke(.semanticColor.border_secondary, style: .init(lineWidth: 1))
+                            .background(ColorPalette.white)
+                            .frame(width: Metric.shareButtonSize, height: Metric.shareButtonSize)
+                            .overlay {
+                                Image("share_icon")
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                    .foregroundStyle(.semanticColor.text_primary)
+                                
+                            }
+                    }
+                    .foregroundStyle(.semanticColor.text_primary)
                 }
-                .foregroundStyle(.semanticColor.text_primary)
 
                 Button {
                     isWebViewPresented = true
-
-                    GA.click_newsletter_carousel(title: card.title, listIndex: index)
+                    if isShareEnabled {
+                        /// 여긴 CarouselModalView 영역
+                        GA.click_newsletter_carousel(title: card.title, listIndex: index)
+                    } else {
+                        /// 여긴 ExploreCardModalView 영역
+                    }
                 } label: {
                     RoundedRectangle(cornerRadius: Metric.nextButtonCornerRadius)
                         .stroke(.semanticColor.border_secondary, style: .init(lineWidth: 1))
@@ -123,5 +130,5 @@ struct CarouselCard: View {
 }
 
 #Preview {
-    CarouselCard(card: .stub(), index: 0, pointColor: ColorPalette.pointPink500)
+    CarouselCard(card: .stub(), index: 0, pointColor: ColorPalette.pointPink500, isShareEnabled: true)
 }
